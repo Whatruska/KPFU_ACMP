df1628244d5dfd67359f76e631cc107e3e9c1e5c
Vitalii Dubrovets <vitdub@inbox.ru>
2019-09-18 22:17:58 +0300

    Task_2 added

---
+++ src/Task_2/Main.java
@@ -0,0 +1,20 @@
+package Task_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a == 0){
+            System.out.println(1);
+        } else if (a > 0){
+            System.out.println((1 + a) * a / 2);
+        } else {
+            a = -a;
+            int result = (1 + a)  * a / 2;
+            result = -result;
+            System.out.println(result + 1);
+        }
+    }
+}
