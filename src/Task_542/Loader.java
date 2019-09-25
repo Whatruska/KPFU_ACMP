e52bc9051390f551a98f19b176efd2abec160786
Whatruska <vitdub@inbox.ru>
2019-09-25 18:35:40 +0300

    new tasks added

---
+++ src/Task_542/Loader.java
@@ -0,0 +1,22 @@
+package Task_542;
+
+import java.util.Scanner;
+
+public class Loader {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String resultString = new StringBuilder(Integer.toBinaryString(num)).toString();
+
+        int twoInFactor = 1;
+        int result = 0;
+        for (int i = 0; i < resultString.length(); i++){
+            if (resultString.charAt(i) == '1'){
+                result += twoInFactor;
+            }
+            twoInFactor *= 2;
+        }
+
+        System.out.println(result);
+    }
+}
